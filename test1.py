91096aae10ff8b7d9b93ee03f746daaea6b98078
Kevin <kch34@pitt.edu>
2021-09-08 18:39:53 -0400

    initial test file

---
+++ test1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Kevin Hostler 
+
+Homework 1
+"""
+
+import os
+import nltk
+import re
+import itertools
+import vocabulary
+import newsreader
+import numpy as np
+import scipy
+import sklearn
+import pandas
+
+
